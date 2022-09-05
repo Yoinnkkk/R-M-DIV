7a1710ba33df38006d6cf662badc4b5fbfb104ce
Yoinnkkk <wojpaw2007@gmail.com>
2022-09-05 17:45:07 +0100

    turned the code into a function and cleaned it up

--- animations.js
+++ animations.js
@@ -1,36 +1,3 @@
-const FadeIn = [
-    {opacity: 0.25},
-    {opacity: 1}
-]
-const bFadeInM = [
-    {backgroundColor: "#a7a4a4"},
-    {backgroundColor: "#ff6961"}
-]
-
-const bFadeInR = [
-    {backgroundColor: "#a7a4a4"},
-    {backgroundColor: "#77dd77"}
-]
-const bFadeInRM = [
-    {backgroundColor: "#77dd77"},
-    {backgroundColor: "#ff6961"}
-]
-const bFadeInMR = [
-    {backgroundColor: "#ff6961"},
-    {backgroundColor: "#77dd77"}
-]
-const FadeOut = [
-    {opacity: 1},
-    {opacity: 0}
-]
-const bFadeOutM = [
-    {backgroundColor: "#ff6961"},
-    {backgroundColor: "#a7a4a4"}
-]
-const bFadeOutR = [
-    {backgroundColor: "#77dd77"},
-    {backgroundColor: "#a7a4a4"}
-]
 const Timing = {
     duration: 500,
     iterations: 1
--- main.js
+++ main.js
@@ -1,102 +1,91 @@
 
+function CreateWindow(position = [0,0], options) {
+    // Pos = [x,y]
+    var top = position[0] + "px", left = position[1] + "px";
+    /* Options = 
+    [
+        0 parent = parent id || required
+        1 movable = bool || default true
+        2 resizable = bool || default true
+        3 text = string || placeholder || optional
+    ]
+    */
+
+    // Default variables
+    options[1] = (typeof options[1] !== 'undefined') ? options[1] : true
+    options[2] = (typeof options[2] !== 'undefined') ? options[2] : true
+    options[3] = (typeof options[3] !== 'undefined') ? options[3] : ""
+
+    // Variables
+    var pos1=0, pos2=0, pos3=0, pos4=0, x=0, y=0;
+    // Create window
+    var Window = document.createElement("div");
+    Window.classList.add("window");
+    document.getElementById(options[0]).appendChild(Window);
+    Window.style.cursor = "move";
+    Window.style.top = top;
+    Window.style.left = left;
+    var p = document.createElement("p");
+    p.classList.add("noselect");
+    p.textContent = options[3]
+    Window.appendChild(p);
+    var resizeaction = document.createElement("div");
+    resizeaction.classList.add("resize");
+    Window.appendChild(resizeaction);
+    // Catch the mouse and redirect it
+    if (options[1] != false) {
+    Window.onmousedown = function(event){catchmousedown(event, 1)};
+    }
+    if (options[2] != false) {
+    resizeaction.onmousedown = function(event){catchmousedown(event, 2)};
+    }
+    function catchmousedown(event, status){
+        if (status == 1) {
+            pos3 = event.pageX;
+            pos4 = event.pageY;
+            document.onmousemove = function(event){mouseHandler(event, 1)};
+            document.onmouseup = function() {
+                document.onmousemove = null;
+                document.onmouseup = null;
+            };
+        } else if (status == 2) {
+            event.stopPropagation();
+            x = event.pageX;
+            y = event.pageY;
+            startwidth = Window.offsetWidth;
+            startheight = Window.offsetHeight;
+            document.onmousemove = function(event){mouseHandler(event, 2)};
+            document.onmouseup = function() {
+                document.onmousemove = null;
+                document.onmouseup = null;
+            };    
+        }
+    }
+    function mouseHandler(event, status){
+        if (status == 1) {
+            // Window Movement
+            pos1 = pos3 - event.pageX;
+            pos2 = pos4 - event.pageY;
+            pos3 = event.pageX;
+            pos4 = event.pageY;
+            Window.style.top = (Window.offsetTop - pos2) + "px";
+            Window.style.left = (Window.offsetLeft - pos1) + "px";
+        } else if (status == 2) {
+            // Window Resize
+            if (Window.offsetWidth > Window.offsetHeight) {
+                p.style.fontSize = Window.offsetHeight/3 + "px";
+            } else {
+                p.style.fontSize = Window.offsetWidth/3 + "px";
+            }
+            
+            Window.style.height = startheight + event.pageY - y + "px";
+            Window.style.width = startwidth + event.pageX - x + "px";
+        }
+    }
+}
+
 
 
 window.addEventListener('load', function () {
-    var pos1=0, pos2=0, pos3=0, pos4=0, x=0, y=0, width=0, height=0;
-    var block = document.getElementById("test");
-    var resizeaction = document.getElementById("resize");
-    var p = block.children[1]
-    // Block Movement
-    block.onmouseover = catchmouseover;
-    block.onmousedown = catchmousedown;
-    block.style.cursor = "move";
-    function catchmouseover() {
-        p.textContent = "Move Me";
-        p.style.opacity = 1;
-        p.animate(FadeIn, Timing);
-        if (block.style.backgroundColor == "#77dd77") {
-            block.animate(bFadeInRM, Timing);
-            block.style.backgroundColor = "#ff6961";
-        } else {
-            block.animate(bFadeInM, Timing);
-            block.style.backgroundColor = "#ff6961";
-        }
-        block.onmouseleave = function(){
-            p.animate(FadeOut, Timing)
-            p.style.opacity = 0;
-            block.animate(bFadeOutM, Timing);
-            block.style.backgroundColor = "#a7a4a4";
-            block.onmouseleave = null;
-        }
-    }
-
-    function catchmousedown(event){
-        pos3 = event.pageX;
-        pos4 = event.pageY;
-        document.onmousemove = mouseHandler;
-        document.onmouseup = clear;
-    }
-    function mouseHandler(event){
-        pos1 = pos3 - event.pageX;
-        pos2 = pos4 - event.pageY;
-        pos3 = event.pageX;
-        pos4 = event.pageY;
-        block.style.top = (block.offsetTop - pos2) + "px";
-        block.style.left = (block.offsetLeft - pos1) + "px";
-        //block.style.top = event.pageX - block.offsetTop + "px";
-        //block.style.left = event.pageY - block.offsetLeft + "px";
-    }
-    function clear(event){
-        document.onmousemove = null;
-        document.onmouseup = null;
-    }
-
-    // Block Resize
-    resizeaction.onmousedown = resizemousedown;
-    resizeaction.onmouseover = resizemouseover;
-    function resizemouseover(event) {
-        event.stopPropagation();
-        p.textContent = "Resize Me";
-        p.style.opacity = 1
-        block.animate(bFadeInR, Timing);
-        if (block.style.backgroundColor == "#ff6961") {
-            block.animate(bFadeInMR, Timing);
-            block.style.backgroundColor = "#77dd77";
-        } else {
-            block.animate(bFadeInR, Timing);
-            block.style.backgroundColor = "#77dd77";
-        }
-        block.onmouseleave = function(){
-            p.animate(FadeOut, Timing)
-            p.style.opacity = 0
-            block.animate(bFadeOutR, Timing);
-            block.style.backgroundColor = "#a7a4a4";
-            block.onmouseleave = null;
-        }
-    }
-
-    function resizemousedown(event) {
-        event.stopPropagation();
-        x = event.pageX;
-        y = event.pageY;
-        startwidth = block.offsetWidth;
-        startheight = block.offsetHeight;
-        document.onmousemove = resizemousemove;
-        document.onmouseup = clearresize;
-        
-    }
-    function resizemousemove(event) {
-        if (block.offsetWidth > block.offsetHeight) {
-            p.style.fontSize = block.offsetHeight/3 + "px";
-        } else {
-            p.style.fontSize = block.offsetWidth/3 + "px";
-        }
-        
-        block.style.height = startheight + event.pageY - y + "px";
-        block.style.width = startwidth + event.pageX - x + "px";
-    }
-    function clearresize() {
-        document.onmousemove = null;
-        document.onmouseup = null;
-    }
+    CreateWindow([15, 15], ["canvas", true, true])
 })
