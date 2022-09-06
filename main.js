6f269fb1eeaf496b414e57106630ff8b8b79839d
Yoinnkkk <wojpaw2007@gmail.com>
2022-09-06 18:48:49 +0100

    added functionality

--- main.js
+++ main.js
@@ -1,42 +1,81 @@
 
-function CreateWindow(position = [0,0], options) {
-    // Pos = [x,y]
-    var top = position[0] + "px", left = position[1] + "px";
+function CreateWindow(position = {x:0,y:0}, options) {
+    // Pos = {x,y}
+    var top = position.x + "px", left = position.y + "px";
+
     /* Options = 
-    [
-        0 parent = parent id || required
+    {
+        0 parentid = parent id || default body
         1 movable = bool || default true
         2 resizable = bool || default true
-        3 text = string || placeholder || optional
-    ]
+        3 text = string || default ""
+        4 dragArea = option || full or title || default full
+        5 title = string || default ""
+        6 titleColor = hex || default #AEC6CF
+    }
     */
 
     // Default variables
-    options[1] = (typeof options[1] !== 'undefined') ? options[1] : true
-    options[2] = (typeof options[2] !== 'undefined') ? options[2] : true
-    options[3] = (typeof options[3] !== 'undefined') ? options[3] : ""
+    options.movable = (typeof options.movable !== 'undefined') ? options.movable : true
+    options.resizable = (typeof options.resizable !== 'undefined') ? options.resizable : true
+    options.text = (typeof options.text !== 'undefined') ? options.text : ""
+    options.dragArea = (typeof options.dragArea !== 'undefined') ? options.dragArea : "full"
+    options.title = (typeof options.title !== 'undefined') ? options.title : ""
 
     // Variables
     var pos1=0, pos2=0, pos3=0, pos4=0, x=0, y=0;
+
     // Create window
     var Window = document.createElement("div");
     Window.classList.add("window");
-    document.getElementById(options[0]).appendChild(Window);
-    Window.style.cursor = "move";
+    (typeof options.parentid !== 'undefined') ? document.getElementById(options.parentid).appendChild(Window) : document.body.appendChild(Window)
     Window.style.top = top;
     Window.style.left = left;
-    var p = document.createElement("p");
-    p.classList.add("noselect");
-    p.textContent = options[3]
-    Window.appendChild(p);
+
+    // Make the title bar
+    var title = document.createElement("div");
+    Window.appendChild(title);
+    title.classList.add("title");
+    title.style.width = Window.offsetWidth+"px";
+    title.style.height = Window.offsetHeight / 16 +"px";
+    
+
+    // Assign the title bar paragraph text node
+    var titleText = document.createElement("p");
+    titleText.classList.add("noselect");
+    titleText.textContent = options.title;
+    titleText.style.fontSize = Window.offsetHeight/20 + "px";
+    title.appendChild(titleText);
+
+    // Assign body paragraph text node
+    var bodyText = document.createElement("p");
+    bodyText.classList.add("noselect");
+    bodyText.id = "bodyID";
+    bodyText.textContent = options.text;
+    Window.appendChild(bodyText);
+
+    // Create resize action in corner#
+    if (options.resizable != false) {
     var resizeaction = document.createElement("div");
     resizeaction.classList.add("resize");
     Window.appendChild(resizeaction);
-    // Catch the mouse and redirect it
-    if (options[1] != false) {
-    Window.onmousedown = function(event){catchmousedown(event, 1)};
     }
-    if (options[2] != false) {
+
+    // Event functions
+    if (options.dragArea !== "title") 
+    {
+        dragTriggerArea = Window
+        Window.style.cursor = "move"
+    } else {
+        dragTriggerArea = title
+        title.style.cursor = "move"
+    }
+
+    // Catch the mouse and redirect it
+    if (options.movable != false) {
+    dragTriggerArea.onmousedown = function(event){catchmousedown(event, 1)};
+    }
+    if (options.resizable != false) {
     resizeaction.onmousedown = function(event){catchmousedown(event, 2)};
     }
     function catchmousedown(event, status){
@@ -73,19 +112,21 @@ function CreateWindow(position = [0,0], options) {
         } else if (status == 2) {
             // Window Resize
             if (Window.offsetWidth > Window.offsetHeight) {
-                p.style.fontSize = Window.offsetHeight/3 + "px";
+                bodyText.style.fontSize = Window.offsetHeight/3 + "px";
+                titleText.style.fontSize = Window.offsetHeight/20 + "px";
             } else {
-                p.style.fontSize = Window.offsetWidth/3 + "px";
+                bodyText.style.fontSize = Window.offsetWidth/3 + "px";
             }
-            
             Window.style.height = startheight + event.pageY - y + "px";
             Window.style.width = startwidth + event.pageX - x + "px";
+            title.style.width = Window.offsetWidth+"px";
+            title.style.height = Window.offsetHeight / 16 +"px";
         }
     }
 }
 
 
-
+// Testing window creator
 window.addEventListener('load', function () {
-    CreateWindow([15, 15], ["canvas", true, true])
+    CreateWindow({x:15,y:15}, {parentid:"canvas",resizable:true,movable:true, title:"Window", dragArea:"title"})
 })
